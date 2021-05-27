9391ea292fb4cbad61cb534f6d3026ba273988b3
Andrew Dibble <andrew.dibble@doctolib.com>
2021-05-27 20:43:26 +0200

    Update

--- src/helpers/verifyPayload.ts
+++ src/helpers/verifyPayload.ts
@@ -2,25 +2,20 @@ import * as crypto from 'crypto';
 import { IncomingMessage } from 'http';
 import { CheckSuitePayload } from '../types';
 
-const verifyPayload = (req: IncomingMessage): Promise<CheckSuitePayload> => new Promise((resolve, reject) => {
-  let data = Buffer.alloc(0);
+const verifyPayload = async (req: IncomingMessage): Promise<CheckSuitePayload> => {
+  let data = '';
   const hmac = crypto.createHmac('sha1', process.env.GITHUB_SECRET as string);
 
-  req.on('data', (chunk) => {
+  for await (const chunk of req) {
     hmac.update(chunk);
-    data = Buffer.concat([data, chunk]);
-  });
+    data += chunk;
+  }
 
-  req.on('end', () => {
-    const bodySignature = Buffer.from(`sha1=${hmac.digest().toString('hex')}`);
-    const headerSignature = Buffer.from(req.headers['x-hub-signature'] as string);
-    try {
-      crypto.timingSafeEqual(bodySignature, headerSignature);
-      return resolve(JSON.parse(data.toString('utf8')));
-    } catch (e) {
-      return reject(e);
-    }
-  });
-});
+  const bodySignature = Buffer.from(`sha1=${hmac.digest().toString('hex')}`);
+  const headerSignature = Buffer.from(req.headers['x-hub-signature'] as string);
+
+  crypto.timingSafeEqual(bodySignature, headerSignature);
+  return JSON.parse(data);
+};
 
 export default verifyPayload;
